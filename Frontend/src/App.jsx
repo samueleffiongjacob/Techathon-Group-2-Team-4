2403bf039468b3177f3159957708f2522d006abf
unknown <Buzo>
2022-08-20 14:19:16 +0100

    CREATED A BOILER PLATE

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {FiFacebook} from 'react-icons/fi'
+
+const App = () => {
+  return (
+    <div>App
+
+      <FiFacebook/>
+    </div>
+  )
+}
+
+export default App
